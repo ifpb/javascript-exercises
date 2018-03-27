8d5142e81b3d50f1d0e2d13b56b4b09d7f08cf8e
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2018-03-27 20:34:05 -0300

    irrf equation

--- ecma/response/basic/irrf-2017.js
+++ ecma/response/basic/irrf-2017.js
@@ -1,8 +1,13 @@
-// Até 1.903,98 | – | –
-// De 1.903,99 até 2.826,65 | 7,5| 142,80
-// De 2.826,66 até 3.751,05 | 15,0 | 354,80
-// De 3.751,06 até 4.664,68 | 22,5 | 636,13
-// Acima de 4.664,68	| 27,5 | 869,36
+// Até 1.903,98             | –          | –
+// De 1.903,99 até 2.826,65 | 7,5        | 142,80
+// De 2.826,66 até 3.751,05 | 15,0       | 354,80
+// De 3.751,06 até 4.664,68 | 22,5       | 636,13
+// Acima de 4.664,68	      | 27,5       | 869,36
+// deduction by dependent   | R$ 189, 59 |
+
+// http://www.receita.fazenda.gov.br/aplicacoes/atrjo/simulador/simulador.asp?tipoSimulador=M
+// https://www.sitecontabil.com.br/noticias/artigo.php?id=2272
+// (salary - dependents - INSS) x tax - deduction
 
 let salary = 3500.5
 let tax
